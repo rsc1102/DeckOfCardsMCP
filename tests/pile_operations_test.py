e4ce31a05d979583b7354b6a5e398acc932bc6e1
rsc1102 <rohitchaudhari1102@gmail.com>
2025-10-25 01:35:17 -0400

    Added test file

---
+++ tests/pile_operations_test.py
@@ -0,0 +1,83 @@
+import pytest
+from main import create_deck as create_deck_tool
+from main import draw_cards as draw_cards_tool
+from main import add_to_pile as add_to_pile_tool
+from main import draw_from_pile as draw_from_pile_tool
+from main import shuffle_pile as shuffle_pile_tool
+from main import list_pile_cards as list_pile_cards_tool
+from pydantic_models import (
+    DeckSchema,
+    DrawCardSchema,
+    AddToPileSchema,
+    PileWithoutCardDetailsSchema,
+    ShufflePileSchema,
+    ListPilesSchema,
+    PileWithCardDetailsSchema
+)
+
+create_deck = create_deck_tool.fn
+draw_cards = draw_cards_tool.fn
+add_to_pile = add_to_pile_tool.fn
+draw_from_pile = draw_from_pile_tool.fn
+shuffle_pile = shuffle_pile_tool.fn
+list_pile_cards = list_pile_cards_tool.fn
+
+
+async def generate_new_deck():
+    deck_count = 1
+    shuffled = False
+    response = await create_deck(deck_count=deck_count, shuffled=shuffled)
+    assert type(response) is DeckSchema
+    assert response.remaining == 52
+    assert not response.shuffled
+    return response.deck_id
+
+
+async def generate_new_pile():
+    new_deck_id = await generate_new_deck()
+    response = await draw_cards(deck_id=new_deck_id, count=10)
+    assert type(response) is DrawCardSchema
+    assert response.remaining == 42
+    assert not response.shuffled
+
+    drawn_cards = response.cards
+    encoded_drawn_cards = [x.code for x in drawn_cards]
+    pile_name = "my_pile"
+    response = await add_to_pile(
+        deck_id=new_deck_id, pile_name=pile_name, cards=encoded_drawn_cards
+    )
+    assert type(response) is AddToPileSchema
+    assert response.deck_id == new_deck_id
+    assert response.remaining == 42
+    assert response.piles[pile_name] == PileWithoutCardDetailsSchema(remaining=10)
+    return new_deck_id, pile_name, encoded_drawn_cards
+
+
+@pytest.mark.asyncio
+async def test_shuffle_pile():
+    deck_id, pile_name, _ = await generate_new_pile()
+    response = await shuffle_pile(deck_id=deck_id, pile_name=pile_name)
+    assert type(response) is ShufflePileSchema
+    assert response.remaining == 42
+    assert response.piles[pile_name] == PileWithoutCardDetailsSchema(remaining=10)
+
+@pytest.mark.asyncio
+async def test_list_cards_in_pile():
+    deck_id, pile_name, cards = await generate_new_pile()
+    response = await list_pile_cards(deck_id=deck_id,pile_name=pile_name)
+    assert type(response) is ListPilesSchema
+    assert response.remaining == 42
+    assert type(response.piles[pile_name]) is PileWithCardDetailsSchema
+    assert response.piles[pile_name].remaining == 10
+    listed_cards = response.piles[pile_name].cards
+    encoded_listed_cards = [x.code for x in listed_cards]
+    assert set(encoded_listed_cards) == set(cards)
+
+
+
+
+
+
+
+
+    
